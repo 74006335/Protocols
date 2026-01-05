e1f2ac41fcbec60ed2ad56561b2c6fdd731f6355
74006335 <ikramc2006@gmail.com>
2026-01-05 23:03:48 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,15 @@
+const sections = document.querySelectorAll(".reveal");
+
+function revealOnScroll() {
+  sections.forEach(section => {
+    const top = section.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+
+    if (top < windowHeight - 100) {
+      section.classList.add("active");
+    }
+  });
+}
+
+window.addEventListener("scroll", revealOnScroll);
+revealOnScroll();
